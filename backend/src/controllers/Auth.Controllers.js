7636fc5f44b44beed4035d9dee26adbb64421015
Sheice <75450007+Sheice@users.noreply.github.com>
2022-09-26 09:59:28 -0500

    create route auth and controller.auth

--- backend/src/controllers/Auth.Controllers.js
+++ backend/src/controllers/Auth.Controllers.js
@@ -1,3 +1,11 @@
 export const register = (req, res) => {
     res.json('prueba')
 }
+
+// LOGIN
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    console.log(password);
+    res.json('hola pa');
+}
--- backend/src/routes/Auth.js
+++ backend/src/routes/Auth.js
@@ -3,9 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 // import controller
-import {register} from '../controllers/Auth.Controllers.js';
+import {register, login} from '../controllers/Auth.Controllers.js';
 
 router.get('/register',register);
+
+// login
+router.post('/login', login);
 
 
 export default router;
