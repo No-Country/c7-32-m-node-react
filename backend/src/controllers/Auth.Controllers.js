e771f90ffa67b89c18da169f47ea62ff0c5ebfca
Sheice <75450007+Sheice@users.noreply.github.com>
2022-09-26 12:21:28 -0500

    middleware of passport to protect routes

--- backend/src/controllers/Auth.Controllers.js
+++ backend/src/controllers/Auth.Controllers.js
@@ -6,5 +6,27 @@ import { comparePassword } from "../utils/encryptPassword.js";
 // LOGIN
 
 export const login = async (req, res) => {
-   res.json('prueba');
+    const { email, password } = req.body;
+
+    if (!email || !password || email.length === 0 || password.length === 0) {
+      return res.status(400).json({ message: "Complete todos los campos" });
+    }
+  
+    const userFound = await User.findOne({ where: { email } });
+  
+    if (!userFound) {
+      return res.status(400).json({ msg: `El mail no está registrado` });
+    }
+  
+    const checkPassword = await comparePassword(password, userFound.password);
+  
+    if (!checkPassword) {
+      return res.status(400).json({ msg: `Contraseña incorrecta` });
+    }
+  
+    const token = jwt.sign({ id: userFound.id }, process.env.PASSWORD_TOKEN, {
+      expiresIn: 60 * 60 * 24, // 24 hours
+    });
+  
+    res.json({ token, message: "Logged correctly " });
 }
