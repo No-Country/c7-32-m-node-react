18d9817556f8037125b63debb7720b79deb5be13
Sheice <75450007+Sheice@users.noreply.github.com>
2022-09-26 13:55:38 -0500

    add try catch to consult of db

--- backend/src/utils/encryptPassword.js
+++ backend/src/utils/encryptPassword.js
@@ -1,11 +1,19 @@
 import bcrypt from "bcryptjs";
 
 export const encryptPassword = async (password) => {
-  const salt = bcrypt.genSaltSync(10);
-  const hash = await bcrypt.hashSync(password, salt);
-  return hash;
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hashSync(password, salt);
+    return hash;
+  } catch (error) {
+    return errror;
+  }
 };
 
 export const comparePassword = async (password, confirmPassword) => {
-  return await bcrypt.compareSync(password, confirmPassword);
+  try {
+    return await bcrypt.compareSync(password, confirmPassword);
+  } catch (error) {
+    return error
+  }
 };
