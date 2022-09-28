0e01354d8b6fa18494df54151eaf428e0d98541c
fdlaurentis <fdlaurentis@gmail.com>
2022-09-28 11:28:52 -0500

    log screen not working yet

--- front-end/wallet-project/src/App.jsx
+++ front-end/wallet-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { React } from 'react'
 import { BrowserRouter as Browser, Routes, Route   } from 'react-router-dom'
 import './App.css'
+import LoginContainer from './components/container/LoginContainer'
+import Register from './components/container/Register.Container'
 
 function App() {
 
@@ -8,8 +10,8 @@ function App() {
     <div className="App">
       <Browser>
         <Routes>
-          <Route />
-          <Route />
+          <Route path='/login' element = { <LoginContainer /> }/>
+          <Route path='/register' element ={ <Register /> } />
           <Route />
           <Route />
           <Route />
--- front-end/wallet-project/src/components/container/LoginContainer.jsx
+++ front-end/wallet-project/src/components/container/LoginContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginContainer = () => {
   return (
     <div>
-      
+      <h1 style={{color:'black'}}>hello word</h1>
     </div>
   )
 }
