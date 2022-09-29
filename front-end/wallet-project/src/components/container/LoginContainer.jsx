dff3b257864a3244a013fce8aa417b5309e2b747
uspaz <Matias-Teijeiro2015@hotmail.com>
2022-09-29 11:21:42 -0300

    google auth and data validation

--- front-end/wallet-project/src/components/container/LoginContainer.jsx
+++ front-end/wallet-project/src/components/container/LoginContainer.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
+import { GoogleLogin } from 'react-google-login'
+import { gapi } from 'gapi-script'
 
 import '../../styles/Login.css'
 import logo from '../../assets/images/Logo-bg-black.png'
 import login_svg from '../../assets/images/Wallet_Monochromatic.svg'
+import axios from 'axios'
 
 const LoginSchema = Yup.object({
   email: Yup.string()
@@ -20,10 +23,31 @@ const LoginSchema = Yup.object({
 const LoginContainer = () => {
 
   const { register, handleSubmit, formState:{ errors } } = useForm( { resolver: yupResolver(LoginSchema) } )  
+  const clientID = '788418791012-cg4g59fivrprdgm4eq9pkgv4fmr8474f.apps.googleusercontent.com'
 
-  const submit = (e) =>{
-    e.preventDefault()
+  const submit = (info) =>{
+    axios({
+      method: 'get',
+      url: 'http:localhost:5000/api/login',
+      data: info
+    })
+  }
+  
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientID,
+        scope: ''
+      })
+    }
+    gapi.load('client:auth2', initClient)  
+  }, [])
 
+  const onSuccess = (res) => {
+    console.log('success:', res)
+  }
+  const onFailure = (err) => {
+    console.log('failed:', err)
   }
 
   return (
@@ -51,11 +75,11 @@ const LoginContainer = () => {
           
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' placeholder='example@example.com' {...register ('email') }/>
-            <p>{errors.email?.message}</p>
+            <p className='error'>{errors.email?.message}</p>
 
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' placeholder='Password...' {...register ('password') }/>
-            <p>{errors.password?.message}</p>
+            <p className='error'>{errors.password?.message}</p>
 
             <div className='form-warned'>
               <div className='form-check'>
@@ -65,10 +89,18 @@ const LoginContainer = () => {
               <Link to='#' className='link' >¿Olvidaste la contraseña?</Link>
             </div>
             <button type='submit' className='btn btn-login' >Inicia Sesión</button>
-            <button type='button' className='btn btn-google'>Inicia sesión con Google</button>
+            <GoogleLogin
+              className='btn-google'
+              clientId={clientID}
+              buttonText="Inicia Sesión con Google"
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy={'single_host_origin'}
+              isSignedIn={true}
+            />
           </form>
           
-          <span>¿No tiene una cuenta? <Link className='link' to='/register'>Únase ahora</Link></span>
+          <span className='register'>¿No tiene una cuenta? <Link className='link' to='/register'>Únase ahora</Link></span>
         </div>   
 
       </div>
