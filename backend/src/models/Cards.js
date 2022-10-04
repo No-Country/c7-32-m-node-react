f2c4c652a5203cb18bb7e31341e67b69d9c177e1
Adrian <esteban.adrian.gonz@gmail.com>
2022-10-04 10:05:53 -0300

    Fix numbers from INT to BIGINT

--- backend/src/models/Cards.js
+++ backend/src/models/Cards.js
@@ -1,44 +1,48 @@
-import {sequelize} from '../db/db.js';
-import {DataTypes} from 'sequelize';
+import { sequelize } from '../db/db.js';
+import { DataTypes } from 'sequelize';
 
 export const Card = sequelize.define('Card', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-      unique: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    surname: {
-      type: DataTypes.STRING
-    },
-    number: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true
-    },
-    exp_date: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    issue_date: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    cvv: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    timestamps: true
-  });
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    unique: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  surname: {
+    type: DataTypes.STRING
+  },
+  number: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    unique: true
+  },
+  exp_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  issue_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  cvv: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  id_user: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  timestamps: true
+});
 
   // ASOCIACIONES
