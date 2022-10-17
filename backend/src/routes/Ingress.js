e24a24718f70fe0df4c947e0894a5f88b53c4489
Adrian <esteban.adrian.gonz@gmail.com>
2022-10-17 09:12:40 -0300

    +CJ_BK auth routes

--- backend/src/routes/Ingress.js
+++ backend/src/routes/Ingress.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { Ingress } from '../controllers/Ingresos.Controllers.js';
+import passport from 'passport';
 
 const router = Router();
 
-router.post('/:idUser/ingress', Ingress);
+router.post('/:idUser/ingress', passport.authenticate('jwt', { session: false }), Ingress);
 
 export default router;
--- backend/src/routes/Transference.js
+++ backend/src/routes/Transference.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+import passport from 'passport';
 
 const router = Router();
 
-// controllers
 import { postTransf } from '../controllers/Transference.Controller.js';
 
-router.post('/user/:userId/transference', postTransf);
+router.post('/user/:userId/transference', passport.authenticate('jwt', { session: false }), postTransf);
 
 export default router;
