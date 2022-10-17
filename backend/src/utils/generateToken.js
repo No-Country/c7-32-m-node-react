4d8cf689c55bebc4968fc3b9f7a70769128bc95f
Adrian <esteban.adrian.gonz@gmail.com>
2022-10-17 09:11:33 -0300

    Fix return

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,8 +1,7 @@
 import jwt from "jsonwebtoken";
 
-
 export const generateToken = (id) => {
-    jwt.sign({ id: id }, process.env.PASSWORD_TOKEN, {
+    return jwt.sign({ id: id }, process.env.PASSWORD_TOKEN, {
         expiresIn: 60 * 60 * 24, // 24 hours
       });
 }
