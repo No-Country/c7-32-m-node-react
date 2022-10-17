d33c933c2f1c83415dbbe8586d567f2c2e77b259
Adrian <esteban.adrian.gonz@gmail.com>
2022-10-17 14:54:37 -0300

    +vercel.json

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 import { config } from "dotenv";
 config({path: '.env'});
+import mysql2 from "mysql2";
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME || "",
@@ -9,6 +10,7 @@ export const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectModule: mysql2,
     dialectOptions: {
       ssl: {
         require: true,
