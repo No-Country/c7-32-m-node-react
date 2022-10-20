703eabf8e29f7da5ea152e3fd489838ca2964186
Sheice <75450007+Sheice@users.noreply.github.com>
2022-10-20 10:46:59 -0300

    show ingresos in dashboard

--- frontend/src/components/pure/stats.jsx
+++ frontend/src/components/pure/stats.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUserContext } from '../context/userContext'
 // import PropTypes from 'prop-types'
 
 import money from '../../assets/images/money.png'
 import income from '../../assets/images/income.png'
 import spend from '../../assets/images/spend.png'
+import { httpsRequest } from '../../assets/config/axios'
 
 const Stats = () => {
   const { user } = useUserContext()
+  const [ingress, setIngress] = useState(0)
+
+  useEffect( () => {
+    async function getHistory() {
+      try {
+        const res = await httpsRequest('get', `http://localhost:5000/api/operations/${user.id}`);
+       
+        let addIngress = 0;
+     for (let index = 0; index < res.data.ingreso.length; index++) {
+      addIngress = addIngress + res.data.ingreso[index].user_amount
+      
+     }
+     setIngress(addIngress)
+
+      } catch (error) {
+        swalAlert('error', 'Oops', error)
+      }
+    }
+    getHistory();
+  },[]);
+
   return (
     <section className='stats-container'>
       <div className='money'>
@@ -17,7 +39,7 @@ const Stats = () => {
       </div>
       <div className='income'>
         <p>Ingresos</p>
-        <p>${}</p>
+        <p>${ingress}</p>
         <img src={income}/>
       </div>
       <div className='spend'>
